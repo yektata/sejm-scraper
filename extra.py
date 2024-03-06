a11a5c3b40257849c454e38edc6ea16af3e3d176
yektata <yekta.oz@sabanciuniv.edu>
2024-03-06 12:36:40 +0100

    modified:   extra.py

--- extra.py
+++ extra.py
@@ -2,7 +2,7 @@ import requests
 from bs4 import BeautifulSoup
 import os
 
-def prime(fileIntegrityOverride=[False,False,False]):
+def prime(fileIntegrityOverride=[False,False,False,False],forceHTMLNoChecks=False):
     
     currentdir = os.getcwd()
     rootdir = os.path.join(currentdir,"content")
@@ -61,7 +61,7 @@ def prime(fileIntegrityOverride=[False,False,False]):
                     pageRaw = requests.get(link)
                     dateLinksTXT.write(f"{link}\n")
                     if pageRaw.status_code == 200:
-                        print(f"got link id: {link[-4:]}")
+                        print(f"got link id: {link[link.rfind('='):]}")
                         with open(os.path.join(HTMLdir,f"{date}.htm"), 'w', encoding='utf-8') as pageFile:
                             pageFile.write(pageRaw.text)
                     else:
@@ -69,37 +69,38 @@ def prime(fileIntegrityOverride=[False,False,False]):
                 else:
                     print(f"have link id: {link[-4:]}")
 
-        for subpage in os.listdir(HTMLdir):
-            print(subpage)
-            if subpage[-3:] != "htm":
-                break
-            with open(os.path.join(HTMLdir,subpage), 'r', encoding='utf-8') as subpageFile:
-                subpageSoup = BeautifulSoup(subpageFile, 'html.parser')
-                subpageBody = subpageSoup.find(name='div', attrs={"id":"contentBody"}).find('tbody')
-                for article in subpageBody.find_all("tr"):
-                    sublink = article.find("td")
-                    sublinkTime = sublink.find_next("td")
-                    sublink = f"{baseURL}{sublink.find('a').get('href')}"
-                    sublinkTime = sublinkTime.text
-                    if fileIntegrityOverride[2] or (not os.path.exists(os.path.join(voteHTMLdir,"vote-links-downloaded.txt"))):
-                        with open(os.path.join(voteHTMLdir,"vote-links-downloaded.txt"), 'w'):
-                            pass
-                    vote_existing_links = open(os.path.join(voteHTMLdir,"vote-links-downloaded.txt"), 'r').read()
-                    with open(os.path.join(voteHTMLdir,"vote-links-downloaded.txt"),'a+', encoding='utf-8') as voteLinksTXT:
-                        if not sublink in vote_existing_links:
-                            votePageRaw = requests.get(sublink)
-                            voteLinksTXT.write(f"{sublink}\n")
-                            if votePageRaw.status_code == 200:
-                                print(f"got link id: {sublink[-3:]}")
-                                with open(os.path.join(voteHTMLdir,f"{subpage[:-4]}-{sublinkTime.replace(':','-')}.htm"), 'w', encoding='utf-8') as votePageFile:
-                                    votePageFile.write(votePageRaw.text)
+        if not forceHTMLNoChecks:
+            for subpage in os.listdir(HTMLdir):
+                print(subpage)
+                if subpage[-3:] != "htm":
+                    break
+                with open(os.path.join(HTMLdir,subpage), 'r', encoding='utf-8') as subpageFile:
+                    subpageSoup = BeautifulSoup(subpageFile, 'html.parser')
+                    subpageBody = subpageSoup.find(name='div', attrs={"id":"contentBody"}).find('tbody')
+                    for article in subpageBody.find_all("tr"):
+                        sublink = article.find("td")
+                        sublinkTime = sublink.find_next("td")
+                        sublink = f"{baseURL}{sublink.find('a').get('href')}"
+                        sublinkTime = sublinkTime.text
+                        if fileIntegrityOverride[2] or (not os.path.exists(os.path.join(voteHTMLdir,"vote-links-downloaded.txt"))):
+                            with open(os.path.join(voteHTMLdir,"vote-links-downloaded.txt"), 'w'):
+                                pass
+                        vote_existing_links = open(os.path.join(voteHTMLdir,"vote-links-downloaded.txt"), 'r').read()
+                        with open(os.path.join(voteHTMLdir,"vote-links-downloaded.txt"),'a+', encoding='utf-8') as voteLinksTXT:
+                            if not sublink in vote_existing_links:
+                                votePageRaw = requests.get(sublink)
+                                voteLinksTXT.write(f"{sublink}\n")
+                                if votePageRaw.status_code == 200:
+                                    print(f"got link id: {sublink[sublink.rfind('='):]}")
+                                    with open(os.path.join(voteHTMLdir,f"{subpage[:-4]}-{sublinkTime.replace(':','-')}.htm"), 'w', encoding='utf-8') as votePageFile:
+                                        votePageFile.write(votePageRaw.text)
+                                else:
+                                    print(f"The page gave a status code of {votePageRaw.status_code}!\nExpected 200.\nThe link provided is:\n{sublink}\nContinuing download process.\n")
                             else:
-                                print(f"The page gave a status code of {votePageRaw.status_code}!\nExpected 200.\nThe link provided is:\n{sublink}\nContinuing download process.\n")
-                        else:
-                            print(f"have link id: {sublink[-3:]}")
+                                print(f"have link id: {sublink[-3:]}")
 
         for pdfpage in os.listdir(voteHTMLdir):
-            if subpage[-3:] != "pdf":
+            if pdfpage[-3:] != "htm":
                 break
             with open(os.path.join(voteHTMLdir,pdfpage), 'r', encoding='utf-8') as pdfHTML:
                 pdfSoup = BeautifulSoup(pdfHTML, 'html.parser')
@@ -113,12 +114,12 @@ def prime(fileIntegrityOverride=[False,False,False]):
                         pdfPageRaw = requests.get(pdflink)
                         if pdfPageRaw.status_code == 200:
                             pdfLinksTXT.write(f"{pdflink}\n")
-                            print(f"got link id: {pdflink[-3:]}")
-                            with open(os.path.join(PDFdir,f"{pdfpage}.pdf"), 'wb', encoding='utf-8') as pdfPageFile:
+                            print(f"got link id: {pdflink[pdflink.rfind('&'):]}")
+                            with open(os.path.join(PDFdir,f"{pdfpage[:-4]}.pdf"), 'wb') as pdfPageFile:
                                 pdfPageFile.write(pdfPageRaw.content)
                         else:
                             print(f"The page gave a status code of {pdfPageRaw.status_code}!\nExpected 200.\nThe link provided is:\n{pdflink}\nContinuing download process.\n")
                     else:
                         print(f"have link id: {pdflink[-3:]}")
 
-prime()
+prime(forceHTMLNoChecks=True)
